ff5cdd2f331e3a26997ea706a7c6ac06d9519bfd
Milan Heimschild <milan@heimschild.com>
2016-03-10 18:06:35 +0100

    examples, syntax highlighting

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var Reveal = require('reveal.js');
 var Markdown = require('reveal.js/plugin/markdown/markdown');
-require('reveal.js/plugin/markdown/marked');
+var hljs = require('highlightjs/highlight.pack.js');
+require('highlightjs/styles/railscasts.css');
+hljs.initHighlightingOnLoad();
 require('reveal.js/css/reveal.css');
 require('reveal.js/css/theme/black.css');
 //require('reveal/theme/default.css');
-
-console.log('---------------------');
-console.log('---------------------');
-
-// Reveal.initialize();
 
 Markdown.initialize();
 
